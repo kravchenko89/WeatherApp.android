18f84ddb71bb29e7ba001f7e32a8e6970fa67eaf
kravchenko89 <symkin2007@gmail.com>
2021-10-30 12:20:29 +0300

    add view model

--- app/src/main/java/ru/kravchenko/weatherapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/kravchenko/weatherapp/viewmodel/MainViewModel.kt
@@ -2,11 +2,46 @@ package ru.kravchenko.weatherapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 import ru.kravchenko.weatherapp.model.WeatherModel
 import ru.kravchenko.weatherapp.service.WeatherAPIService
 
 class MainViewModel: ViewModel() {
+    private val weatherApiService = WeatherAPIService()
+    private val disposable = CompositeDisposable()
 
+    val weatherData = MutableLiveData<WeatherModel>()
+    val weatherError = MutableLiveData<Boolean>()
+    val weatherLoad = MutableLiveData<Boolean>()
+
+
+    fun refreshData(){
+        getDataFromAPI()
+    }
+
+    private fun getDataFromAPI() {
+        weatherLoad.value = true
+        disposable.add(
+            weatherApiService.getDataService()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<WeatherModel>(){
+                    override fun onSuccess(t: WeatherModel) {
+                        weatherData.value = t
+                        weatherError.value = false
+                        weatherError.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        weatherError.value = true
+                        weatherLoad.value = false
+                    }
+
+                })
+        )
+    }
 
 }
